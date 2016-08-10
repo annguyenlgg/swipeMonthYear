1cd35b82e2e99a20b333c040155c1b2fd2138bab
haz <ha.h@neo-lab.vn>
2016-08-10 16:22:41 +0700

    init

---
+++ swipeMonthYear/swipeMonthYear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  swipeMonthYear
+//
+//  Created by AnN on 8/10/16.
+//  Copyright © 2016 AnN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ swipeMonthYear/swipeMonthYear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  swipeMonthYear
+//
+//  Created by AnN on 8/10/16.
+//  Copyright © 2016 AnN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
